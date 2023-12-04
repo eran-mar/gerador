7506eb5dd9bca602b0cae38816b93a6f2016bf0c
eran-mar <eran.ramos@escola.pr.gov.br>
2023-12-04 12:09:14 -0300

    oi

--- App.js
+++ App.js
@@ -1,81 +1,9 @@
-import { useState } from "react";
-import { View, Text, StyleSheet, Image, Touchable, TouchableOpacity} from "react-native";
-import Slider from '@react-native-community/slider'
+import { View } from "react-native";
 
 export default function App(){
-  const [size, setSize] = useState(10)
-
-
-
-  return(
-    <View style={styles.container}>
-      <Image
-        source={require("./src/assets/logo.png")}
-        style={styles.logo}
-      />
-
-      <Text style={styles.title}>{size} caracteres</Text>
-
-      <View style={styles.area}>
-        <Slider
-        style={{ height:50 }}
-        minimumValue={6}
-        maximumValue={20}
-        maximumTrackTintColor="#ff0000"
-        minimumTrackTintColor="#000"
-        thumbTintColor="#392de9"
-        value={size}
-        onValueChange={ (value) => setSize(value.toFixed(0))}
-        />
-      </View>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Gerar senha</Text>
-      </TouchableOpacity>
+    return(
+    <View>
 
     </View>
-
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#F3F3FF",
-    justifyContent: 'center',
-    alignItems:'center'
-  },
-
-  logo:{
-    marginBottom: 60,
-  },
-
-  area:{
-    marginTop: 14,
-    marginBottom: 14,
-    width: "80%",
-    backgroundColor: "#FFF",
-    borderRadius: 8,
-    padding: 6,
-  },
-
-  button:{
-    backgroundColor: "#392de9",
-    width: "80%",
-    height: 50,
-    alignItems:'center',
-    justifyContent: 'center',
-    borderRadius: 8,
-    marginBottom:18,
-  },
-
-  buttonText:{
-    color:"#FFF",
-    fontSize: 20,
-  },
-
-  title:{
-    fontSize: 30,
-    fontWeight: 'bold'
-  }
-})
---
+++ src/pages/paswords/index.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react";
+
+export function Passwords(){
+    return(
+        <View>
+            <Text>Minhas Senhas</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
